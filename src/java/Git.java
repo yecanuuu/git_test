74ac5bf358ce5bbe08c2361178427985834614ab
root <zhengjiahao@11>
2023-05-07 09:35:56 +0800

    first commit

---
+++ src/java/Git.java
@@ -0,0 +1,10 @@
+package java;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("holle git ");
+        System.out.println("holle git ");
+        System.out.println("holle git ");
+    }
+}
+
